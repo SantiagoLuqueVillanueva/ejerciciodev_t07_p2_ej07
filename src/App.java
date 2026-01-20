f38e9b13b1be4af06f3bb6b42667f778db5980a4
Santiago <luquevillanuevasantiago@gmail.com>
2026-01-20 22:16:29 +0100

    A medias

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[][] board = new String[5][4];
+
+        int deathRow = (int)(Math.random()) * 6;
+        int deathColumn = (int)(Math.random()) * 5;
+
+        board[deathRow][deathColumn] = "*";
+
+        boolean correctPos = false;
+        int row = 0;
+        int column = 0;
+
+        do {
+            row = Integer.parseInt(System.console().readLine("Cordenada x: "));
+            column = Integer.parseInt(System.console().readLine("Cordenada y: "));
+
+            if (board[row][column].equals("*")) {
+                System.out.println("Has perdido");
+                correctPos = true;
+            } else {
+                board[row][column] = "X";
+            } 
+        } while (correctPos);
     }
 }
