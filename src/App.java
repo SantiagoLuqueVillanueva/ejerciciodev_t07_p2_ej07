bdbb183406aff9e9840070a522bf89e29d191f1b
Santiago <luquevillanuevasantiago@gmail.com>
2026-01-21 17:32:25 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -2,25 +2,69 @@ public class App {
     public static void main(String[] args) throws Exception {
         String[][] board = new String[5][4];
 
-        int deathRow = (int)(Math.random()) * 6;
-        int deathColumn = (int)(Math.random()) * 5;
+        int treasureRow = (int)(Math.random() * 5);
+        int treasureColumn = (int)(Math.random() * 4);
 
-        board[deathRow][deathColumn] = "*";
+        int mineRow = (int)(Math.random() * 5);
+        int mineColumn = (int)(Math.random() * 4);
 
-        boolean correctPos = false;
-        int row = 0;
-        int column = 0;
+        boolean win = false;
 
         do {
-            row = Integer.parseInt(System.console().readLine("Cordenada x: "));
-            column = Integer.parseInt(System.console().readLine("Cordenada y: "));
+            int playerRow = Integer.parseInt(System.console().readLine("Coordenada x: "));
+            int playerColumn = Integer.parseInt(System.console().readLine("Coordenada y: "));
 
-            if (board[row][column].equals("*")) {
+            if ((playerRow == treasureRow) && (playerColumn == treasureColumn)) {
+                board[treasureRow][treasureColumn] = "*";
+                System.out.println("¡GANASTE!");
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        String cell = board[i][j];
+                        if (cell == null) {
+                            cell = " "; 
+                        }
+                        System.out.printf("%5s", cell);
+                    }
+                    System.out.println();
+                }
+                win = true;
+            } else if ((playerRow == mineRow) && (playerColumn == mineColumn)){
+                board[playerRow][playerColumn] = "&";
                 System.out.println("Has perdido");
-                correctPos = true;
-            } else {
-                board[row][column] = "X";
-            } 
-        } while (correctPos);
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        String cell = board[i][j];
+                        if (cell == null) {
+                            cell = " "; 
+                        }
+                        System.out.printf("%5s", cell);
+                    }
+                    System.out.println();
+                }
+                win = true;
+            }
+            else{
+                board[playerRow][playerColumn] = "X";
+                System.out.println("Sigue intentándolo");
+
+                int distFila = Math.abs(playerRow - mineRow);
+                int distColumna = Math.abs(playerColumn - mineColumn);
+
+                if (distFila <= 1 && distColumna <= 1) {
+                    System.out.println("¡CUIDADO! ¡Hay una mina muy cerca!");
+                }
+                
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        String cell = board[i][j];
+                        if (cell == null) {
+                            cell = " "; 
+                        }
+                        System.out.printf("%5s", cell);
+                    }
+                    System.out.println();
+                }
+            }
+        } while (!win);
     }
 }
